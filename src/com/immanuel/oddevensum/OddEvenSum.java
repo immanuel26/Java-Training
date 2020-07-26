ccb7f506d059d074c5170be9472f2c668948c059
Immanuel Prasad <58379733+Immanuelprasad@users.noreply.github.com>
2020-07-26 21:30:42 +0530

    Java_Core

---
+++ src/com/immanuel/oddevensum/OddEvenSum.java
@@ -0,0 +1,25 @@
+package com.immanuel.oddevensum;
+
+public class OddEvenSum
+{
+    public static void main(String[] args)
+    {
+        int i, j, oddSum = 0, evenSum = 0;
+        for (i = 0; i <= 20; i++)
+            if (i % 2 != 0)
+            {
+                System.out.println(i);
+                oddSum = oddSum + i;
+            }
+        System.out.println("Sum of Odd Numbers " + oddSum);
+        {
+            for (j = 0; j <= 20; j++)
+                if (j % 2 == 0)
+                {
+                    System.out.println(j);
+                    evenSum = evenSum + i;
+                }
+            System.out.println("Sum of Even Numbers " + evenSum);
+        }
+    }
+}
