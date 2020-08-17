5090c582932cdcec86e94910082773bcc78b01f6
Immanuel Prasad <58379733+Immanuelprasad@users.noreply.github.com>
2020-08-17 20:09:41 +0530

    Java_Core

--- src/com/immanuel/java/multi_threading/getting_name_and_setting_priority/GettingNameAndSettingPriorityInThreads.java
+++ src/com/immanuel/java/multi_threading/getting_name_and_setting_priority/GettingNameAndSettingPriorityInThreads.java
@@ -12,8 +12,8 @@ public class GettingNameAndSettingPriorityInThreads
                                       },"Thread Name : Hai");
         Thread t2 = new Thread(() ->  {for (int i = 1; i <= 5; i++)
                                       {
-                                      System.out.println("Hello MyWorld and Priority : " +Thread.currentThread().getPriority());
-                                      try {Thread.sleep(1000);} catch (Exception e) {}
+                                           System.out.println("Hello MyWorld and Priority : " +Thread.currentThread().getPriority());
+                                           try {Thread.sleep(1000);} catch (Exception e) {}
                                       }
                                       },"Thread Name : Hello");
 
